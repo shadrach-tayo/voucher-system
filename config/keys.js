012990409243d678d1de48b52a623386a7621589
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2018-11-01 18:01:47 +0100

    configure google auth and implement login on the server

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientID: '72379212736-ll7hlfsf8p5e27f5cs9j5sq5pfbth3bf.apps.googleusercontent.com',
+  googleClientSecret: 'cy5X3UpHEVerEnOZhPu0OVwF'
+}
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: 'http://localhost:5000/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('accessToken: ', accessToken);
+    console.log('refreshToken: ', refreshToken);
+    console.log('profile: ', profile);
+  }
+));
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome to Voucher system</h1>');
-});
+  res.send('<h1>Welcome to Voucher system</h1>')
+})
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 app.listen(PORT, () => console.log('Server running on port: ', PORT));
