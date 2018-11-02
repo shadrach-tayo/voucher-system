54abe517b258523a29be7eca44c8d35764567597
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2018-11-02 17:02:09 +0100

    feat: Authenticate and create cookie using user profile data
    - create auth routing module to handle authetication routing
    - redirect user to `/dashboard` if authentication was successful

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 const express = require('express');
 const app = express();
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// require authentication routing handler
+// and plug into express app instance
+require('./routes/authRoutes')(app);
+
+// serialize user 
+passport.serializeUser((user, done) => {
+  console.log(user);
+  done(null, user);
+});
+
+// deserialize user
+passport.deserializeUser((user, done) => {
+  console.log(user);
+  done(null, user);
+})
+
+// config for the google strategy when authentication using google
 passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: 'http://localhost:5000/auth/google/callback'
-  }, (accessToken, refreshToken, profile, done) => {
-    console.log('accessToken: ', accessToken);
-    console.log('refreshToken: ', refreshToken);
-    console.log('profile: ', profile);
-  }
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: 'http://localhost:5000/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  console.log('accessToken: ', accessToken);
+  console.log('refreshToken: ', refreshToken);
+  console.log('profile: ', profile);
+  done(null, profile);
+}
 ));
 
-const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to Voucher system</h1>')
-})
+}); 
 
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}))
-
-app.get('/auth/google/callback', passport.authenticate('google'))
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server running on port: ', PORT));
---
+++ routes/authRoutes.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+
+/**
+ * Module to handle authentication routing
+ */
+module.exports = app => {
+    
+  app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }));
+
+  app.get('/auth/google/callback', 
+    passport.authenticate('google', {failureRedirect: '/login'}),
+    (req, res) => {
+      res.redirect('/dashboard')
+    }
+  );
+
+  app.get('/dashboard', (req, res) => {
+    res.send(req.user);
+  })
+}
