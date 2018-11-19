b28e13aab4db8a14845c8a4426f726caacd2a1d2
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2018-11-19 13:26:31 +0100

    chore: Separate client from server
    - setup more environmental variables to work with google cloud

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  googleCallBackURL: 'https://3000-dot-4871052-dot-devshell.appspot.com/auth/google/callback'
 }
--- services/passport.js
+++ services/passport.js
@@ -27,7 +27,7 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
+    callbackURL: keys.googleCallBackURL,
     proxy: true
   }, 
   (accessToken, refreshToken, profile, done) => {
